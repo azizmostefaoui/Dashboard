f70d16bc08076373f53be21f947887d9cbfa6b97
abdelaaziz mostefaoui <azizmostfaouikmk@gmail.com>
2025-02-13 10:42:21 +0100

    Deploy React project

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -111,7 +111,7 @@ const navigate = useNavigate();
       </DrawerHeader>
       <Divider />
 
-      <Avatar sx={{mx:"auto", height:open?  88 :55,width:open?  88 :55,my:1 ,border:"2px solid gray",transition:"0.25s"}} alt="Aziz Sharp" src="public/aziz.png" />
+      <Avatar sx={{mx:"auto", height:open?  88 :55,width:open?  88 :55,my:1 ,border:"2px solid gray",transition:"0.25s"}} alt="Aziz Sharp" src="dist/assets/aziz.png" />
       <Typography align="center" sx={{fontSize: open? 17 :0,transition:"0.25s"}}>abdelaaziz </Typography>
       <Typography align="center" sx={{fontSize: open? 15 :0,transition:"0.25s",color: theme.palette.info.main}}>admin</Typography>
       <Divider />
